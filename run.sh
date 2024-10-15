837b3889437baca0921ea539735433badb1bab94
yo <tyler4077@icloud.com>
2024-10-15 14:53:37 -0400

    made a better run script

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+run_plot() {
+    while true; do
+        read -p "Would you like to plot the data? (y/n): " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer yes (y) or no (n).";;
+        esac
+    done 
+}
+
+gfortran MCIE.f90 -o MCIE -fopenmp
+
+if [ $? -eq 0 ]; then
+    echo "COMPILATION SUCCESSFUL...RUNNING PROGRAM"
+    sleep 1
+    ./MCIE
+    echo "INTEGRAL ESTIMATION COMPLETE"
+fi
+if run_plot; then
+        echo "PLOTTING DATA"
+        python3 dataplot.py
+    else
+        echo "CSV FILE OUTPUT IN DIRECTORY"
+fi
